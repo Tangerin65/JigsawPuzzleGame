ce4927d076f09d4363dc34443e0bdafbed8b29f9
Wang Haoyu <2407728944@qq.com>
2026-03-24 11:40:59 +0800

    开始

---
+++ src/com/jl65/ui/Test.java
@@ -0,0 +1,13 @@
+package com.jl65.ui;
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(603,680);
+        frame.setVisible(true);
+        frame.setTitle("拼图游戏V0.1");
+
+    }
+}
