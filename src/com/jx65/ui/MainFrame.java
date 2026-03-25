adb3bc42c37d405dda283bd040ff55bed19829b3
Wang Haoyu <2407728944@qq.com>
2026-03-25 21:11:02 +0800

    引入图片素材

--- src/com/jx65/ui/MainFrame.java
+++ src/com/jx65/ui/MainFrame.java
@@ -4,9 +4,37 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
-        FrameInit();//界面初始化
-        getBar();//菜单初始化
-        this.setVisible(true);//界面可见
+        //界面初始化
+        FrameInit();
+
+        //菜单初始化
+        getBar();
+
+        //图片初始化
+        initImage();
+
+        //界面可见
+        this.setVisible(true);
+
+
+    }
+
+    private void initImage() {
+        int index=1;//图片名称索引
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                //图片初始化
+                JLabel img_label=new JLabel(new ImageIcon("Image/EMUs/"+index+".png"));//管理容器直接添加图片对象
+                img_label.setBounds(150*j, 150*i, 150, 150);//坐标尺寸设置
+                this.getContentPane().add(img_label);
+                index++;
+            }
+        }
+
+
+
+
+
     }
 
     private void getBar() {
@@ -31,10 +59,12 @@ public class MainFrame extends JFrame {
 
     private void FrameInit() {
         //界面初始化
-        this.setSize(603, 680);
+        this.setSize(603, 660);
         this.setAlwaysOnTop(true);//界面位于最上层
         this.setLocationRelativeTo(null);//界面居中
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//关闭模式：关闭任何窗口时结束虚拟机运行
+        this.setLayout(null);//取消默认布局
+        this.setVisible(true);
         this.setTitle("拼图游戏V0.1:主界面");
     }
 
