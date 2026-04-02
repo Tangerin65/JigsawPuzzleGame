fd0f2ee86cc526f7c4cb99baa209028eb2cc7e09
Wang Haoyu <2407728944@qq.com>
2026-04-02 15:44:17 +0800

    实现键盘监听功能，添加图片向上移动逻辑；修改图片数据结构，增加data数组作为图片排序抽象

--- src/com/jx65/ui/MainFrame.java
+++ src/com/jx65/ui/MainFrame.java
@@ -2,17 +2,26 @@ package com.jx65.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements KeyListener {
+    //数据结构：二维数组data，位置抽象
+    private final Integer[][] data = new Integer[4][4];
+    private int blankX;
+    private int blankY;
+
     public MainFrame() {
         //界面初始化
         FrameInit();
 
-        //菜单初始化
+        //顶部菜单初始化
         getBar();
+
+        //初始化棋盘数据（只执行一次）
+        initData();
 
         //图片初始化
         initImage();
@@ -23,24 +32,40 @@ public class MainFrame extends JFrame {
 
     }
 
-    private void initImage() {
-        int index=1;//图片名称索引
-
-        //打乱图片顺序
-        Integer[]array=new Integer[16];
+    private void initData() {
+        Integer[] array = new Integer[16];
         for (int i = 1; i <= 15; i++) {
-            array[i-1]=i;
+            array[i - 1] = i;
         }
-        Collections.shuffle(Arrays.asList(array));
+        Collections.shuffle(Arrays.asList(array));//打乱图片顺序
+
+        int idx = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                data[i][j] = array[idx++];
+                if (data[i][j] == null) {
+                    blankX = j;
+                    blankY = i;
+                }
+            }
+        }
+    }
+
+    private void initImage() {
+        this.getContentPane().removeAll();//图片格子重绘
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
+                Integer nums = data[i][j];
                 //图片初始化
-                JLabel img_label=new JLabel(new ImageIcon("Image/EMUs/"+array[index-1]+".png"));//管理容器直接添加图片对象
-                img_label.setBounds(150*j+90, 150*i+100, 150, 150);//坐标尺寸设置
+                JLabel img_label;
+                if (nums == null) {
+                    img_label = new JLabel();//空白格子：跳过图片填充
+                } else {
+                    img_label = new JLabel(new ImageIcon("Image/EMUs/" + nums + ".png"));
+                }                img_label.setBounds(150*j+90, 150*i+100, 150, 150);//坐标尺寸设置
                 img_label.setBorder(new BevelBorder(BevelBorder.LOWERED));//边框设置:BevelBorder-凹陷边框
-                this.getContentPane().add(img_label);
-                index++;
+                this.getContentPane().add(img_label);//图片应用至区域
             }
         }
 
@@ -52,9 +77,8 @@ public class MainFrame extends JFrame {
         //this.getContentPane().setComponentZOrder(background, 0);
         // 需要恢复到底层时，改回：
         this.getContentPane().setComponentZOrder(background, this.getContentPane().getComponentCount() - 1);
-
-
-
+        this.getContentPane().revalidate();
+        this.getContentPane().repaint();
     }
 
     private void getBar() {
@@ -79,14 +103,65 @@ public class MainFrame extends JFrame {
 
     private void FrameInit() {
         //界面初始化
-        this.setSize(800, 840);
+        this.setSize(820, 840);
         this.setAlwaysOnTop(true);//界面位于最上层
         this.setLocationRelativeTo(null);//界面居中
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//关闭模式：关闭任何窗口时结束虚拟机运行
         this.setLayout(null);//取消默认布局
+        this.addKeyListener(this);//添加键盘监听事件
         this.setVisible(true);
         this.setTitle("拼图游戏V0.2:主界面");
     }
 
+    //上移规则实现
+    private boolean moveUpByRule() {
+        if (blankY >= 3) {
+            return false;
+        }
+        data[blankY][blankX] = data[blankY + 1][blankX];
+        data[blankY + 1][blankX] = null;
+        blankY++;
+        return true;
+    }
 
+    //图片移动：实现键盘监听事件接口
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // 判断键盘方向, 左上右下对应 37-40
+        int code = e.getKeyCode();
+        switch (code) {
+            case 37: // 左移
+                System.out.println("左");
+                // TODO: 添加左移逻辑
+                break;
+            case 38: // 上移
+                System.out.println("上");
+                if (moveUpByRule()) {
+                    initImage();
+                }
+
+                break;
+            case 39: // 右移
+                System.out.println("右");
+                // TODO: 添加右移逻辑
+                break;
+            case 40: // 下移
+                System.out.println("下");
+                // TODO: 添加下移逻辑
+                break;
+            default:
+                System.out.println("其他按键: " + code);
+                break;
+        }
+    }
 }
